f06923c74f6c95046a8b1d0eea3b55288d125816
SakethMaddi <sakethmaddi@gmail.com>
2025-10-27 22:09:53 +0530

    task

---
+++ class/src/App.jsx
@@ -0,0 +1,18 @@
+import {Component} from "react";
+import {Message} from "./components/dynamic";
+import { CounterApp } from "./components/dynamic";
+import { RandomNum } from "./components/dynamic";
+import { TextMes } from "./components/dynamic";
+
+class App extends Component{
+  render(){ 
+    return <div>
+      {/* <h3>Hello</h3> */}
+      {/* < Message /> */}
+      <CounterApp/>
+      < RandomNum/>
+      <TextMes/>
+  </div>
+  }
+}
+export default App
